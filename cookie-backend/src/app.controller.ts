3d7c7e48155051f39093755bbc0959609841c902
minho <minhob38@gmail.com>
2023-08-09 11:25:55 +0900

    코드 정리

--- cookie-backend/src/app.controller.ts
+++ cookie-backend/src/app.controller.ts
@@ -6,21 +6,6 @@ import { Request, Response } from 'express';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // /**
-  //  * @description cookie 발급
-  //  */
-  // @Post('api/cookie')
-  // postCookie(@Res({ passthrough: true }) res: Response): string {
-  //   res.cookie('my-cookie', 'cookie value :)', {
-  //     // domain: 'vos.land:3000',
-  //     // secure: true,
-  //     // sameSite: 'none',
-  //     httpOnly: true,
-  //   });
-
-  //   return 'cookie created';
-  // }
-
   /**
    * @description cookie 발급
    */
@@ -44,11 +29,8 @@ export class AppController {
     @Req() request: Request,
     @Res({ passthrough: true }) res: Response,
   ): string {
-    // console.log(request);
-    console.log(request.headers['referer']);
     console.log(request.cookies);
     console.log(request.cookies['my-cookie']);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
     return 'cookie checked';
   }
 }
